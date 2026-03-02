948d2521bbbbd52d6ceb1376a8cf6e810d24c5b6
no_one <thebrokenpipe@proton.me>
2026-03-02 14:17:27 +0000

    chore: commit files.

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-137f9e40.js',
+                path_to_root + 'searchindex-c692d697.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
