6991f3dffe648c0b72be24de930d53f949102a98
no_one <thebrokenpipe@proton.me>
2026-05-13 17:13:44 +0100

    done

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-700c46a3.js',
+                path_to_root + 'searchindex-28f51d6f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
