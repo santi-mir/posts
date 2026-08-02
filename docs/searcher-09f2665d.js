2918576be3b8ab1b15b8038b3ec05b551aca41b6
no_one <thebrokenpipe@proton.me>
2026-08-02 16:45:04 +0100

    new section, the right metaphor

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5202a5b5.js',
+                path_to_root + 'searchindex-5dbaa679.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
