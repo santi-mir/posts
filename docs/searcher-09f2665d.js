1a800441ca8fb701e3d7f2f0a70c7bbf020db8be
no_one <thebrokenpipe@proton.me>
2026-08-02 16:34:17 +0100

    remove some paragraphs

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4b9255d0.js',
+                path_to_root + 'searchindex-5202a5b5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
